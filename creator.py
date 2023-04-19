551f85305adc03c798ab25a8e94a5a486c46cef9
Nally11 <118730255+Nally11@users.noreply.github.com>
2023-04-19 14:22:08 +0200

    Create creator.py

---
+++ creator.py
@@ -0,0 +1,185 @@
+'''- its a little bit messy 
+but it works :) and i did it alone just 
+playing around so hope you are kind'''
+
+
+import random
+lucky = random.randint(1,3)
+lucky2 = random.randint(1,3)
+lucky3 = random.randint(1,3)
+lucky4 = random.randint(1,3)
+lucky5 = random.randint(1,3)
+
+
+
+
+stat11 = random.randint(5,10)
+stat12 = random.randint(5,10)
+stat13 = random.randint(5,10)
+stat14 = random.randint(5,10)
+stat15 = random.randint(5,10)
+stat16 = random.randint(5,10)
+stat17 = random.randint(5,10)
+stat18 = random.randint(5,10)
+stat19 = random.randint(5,10)
+stat110 = random.randint(5,10)
+stat113 = random.randint(5,10)
+stat114 = random.randint(5,10)
+stat115 = random.randint(5,10)
+stat116 = random.randint(5,10)
+stat117 = random.randint(5,10)
+stat118 = random.randint(5,10)
+stat119 = random.randint(5,10)
+stat120 = random.randint(5,10)
+stat121 = random.randint(5,10)
+stat112 = random.randint(5,10)
+stat111 = random.randint(5,10)
+stat122 = random.randint(5,10)
+stat123 = random.randint(5,10)
+stat124 = random.randint(5,10)
+stat125 = random.randint(5,10)
+stat126 = random.randint(5,10)
+stat127 = random.randint(5,10)
+stat128 = random.randint(5,10)
+stat129 = random.randint(5,10)
+stat1211 = random.randint(5,10)
+
+
+
+
+
+
+
+
+stat2 = random.randint(15,30)
+stat21 = random.randint(15,30)
+stat22 = random.randint(15,30)
+stat23 = random.randint(15,30)
+stat24 = random.randint(15,30)
+stat25 = random.randint(15,30)
+stat26 = random.randint(15,30)
+stat27 = random.randint(15,30)
+stat28 = random.randint(15,30)
+stat29 = random.randint(15,30)
+stat211 = random.randint(15,30)
+stat212 = random.randint(15,30)
+stat213 = random.randint(15,30)
+stat214 = random.randint(15,30)
+stat215 = random.randint(15,30)
+
+
+
+
+first_name = input("Welcome Traveller, name your 5 Champions -First Name")
+second_name = input("Second Name")
+third_name = input("Third name")
+fourth_name = input("Fourth Name")
+fifth_name = input("Fifth Name")
+
+
+
+if lucky == 1:
+    print (first_name, "is a Warrior!")
+    print("strength:", stat2)
+    print("Intelligence:", stat11)
+    print("health:", stat12)
+
+elif lucky==2:
+    print (first_name,"is a Wizard")
+    print("strength", stat13)
+    print("Intelligence:", stat21)
+    print("health:", stat14)
+
+elif lucky==3:
+    print(first_name,"is a Potato")
+    print("strength", stat15)
+    print("Intelligence:", stat16)
+    print("Health:", stat22)
+
+
+print()
+
+if lucky2 == 1:
+    print (second_name, "is a Warrior!")
+    print("strength:", stat23)
+    print("Intelligence:", stat17)
+    print("health:", stat18)
+
+elif lucky2==2:
+    print (second_name,"is a Wizard")
+    print("strength", stat19)
+    print("Intelligence:", stat24)
+    print("Health:", stat119)
+
+elif lucky2==3:
+    print(second_name,"is a Potato")
+    print("strength", stat11)
+    print("Intelligence:", stat112)
+    print("Health:", stat25) 
+
+print()
+
+if lucky3 == 1:
+    print (third_name, "is a Warrior!")
+    print("strength:", stat26)
+    print("Intelligence:", stat113)
+    print("health:", stat114)
+
+elif lucky3==2:
+    print (third_name,"is a Wizard")
+    print("strength", stat115)
+    print("Intelligence:", stat27)
+    print("Health:", stat116)
+
+
+elif lucky3==3:
+    print(third_name,"is a Potato")
+    print("strength", stat117)
+    print("Intelligence:", stat118)
+    print("Health:", stat28)
+
+print()
+
+if lucky4 == 1:
+    print (fourth_name, "is a Warrior!")
+    print("strength:", stat29)
+    print("Intelligence:", stat119)
+    print("health:", stat112)
+
+elif lucky4==2:
+    print (fourth_name,"is a Wizard")
+    print("strength", stat120)
+    print("Intelligence:", stat211)         ###
+    print("Health:", stat121)
+
+
+elif lucky4==3:
+    print(fourth_name,"is a Potato")
+    print("strength", stat122)
+    print("Intelligence:", stat123)
+    print("Health:", stat212)
+
+print()
+
+if lucky5 == 1:
+    print (fifth_name, "is a Warrior!")
+    print("strength:", stat213)
+    print("Intelligence:", stat124)
+    print("health:", stat125)
+
+elif lucky5==2:
+    print (fifth_name,"is a Wizard")
+    print("strength", stat126)
+    print("Intelligence:", stat214)
+    print("Health:", stat127)
+
+
+elif lucky5==3:
+    print(fifth_name,"is a Potato")
+    print("strength", stat128)
+    print("Intelligence:", stat129)
+    print("Health:", stat215)
+
+print()
+
+print("Happy Adventures")
